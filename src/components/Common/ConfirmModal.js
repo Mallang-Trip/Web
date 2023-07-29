bb6f883cf00dca55c6c96a12b2bafb363f1976e2
DaeHee99 <mokday99@gmail.com>
2023-07-29 16:13:16 +0900

    feat: 로그인 오류 모달

---
+++ src/components/Common/ConfirmModal.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ConfirmModal(props) {
+  const closeModal = () => {
+    document.body.classList.remove("overflow-hidden");
+    props.setShowModal(false);
+  };
+
+  return (
+    <div
+      className={`modal-container fixed top-0 left-0 z-50 w-screen h-screen bg-gray-900 bg-opacity-50 scale-100 flex ${
+        props.showModal ? "active" : ""
+      }`}
+    >
+      <div className="m-auto shadow w-96 rounded-xl">
+        <div className="flex flex-col justify-center h-64 text-center text-black whitespace-pre bg-white rounded-t-xl">
+          {props.message}
+        </div>
+        <button
+          className="w-full h-16 text-lg text-center text-white rounded-b-xl bg-primary"
+          onClick={closeModal}
+        >
+          확인
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ConfirmModal;

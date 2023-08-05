b466632327aa809d745fafb1a892e17ef6eab261
DaeHee99 <mokday99@gmail.com>
2023-08-06 01:16:40 +0900

    fix: 회원가입 동의 아이콘

--- src/components/SignupPage/Agreement.js
+++ src/components/SignupPage/Agreement.js
@@ -1,4 +1,29 @@
 import React, { useEffect, useState } from "react";
+import { ReactComponent as Check } from "../../assets/svg/agree-check.svg";
+import AgreeItem from "./AgreeItem";
+
+const agreementData = [
+  {
+    id: 0,
+    title: "이용약관 동의(필수)",
+  },
+  {
+    id: 1,
+    title: "만 14세 이상 확인(필수)",
+  },
+  {
+    id: 2,
+    title: "개인정보 수집 및 이용 동의(필수)",
+  },
+  {
+    id: 3,
+    title: "마케팅 알림 수신 동의(선택)",
+  },
+  {
+    id: 4,
+    title: "위치기반 서비스 이용약관 동의(선택)",
+  },
+];
 
 function Agreement(props) {
   const [allChecked, setAllChecked] = useState(false);
@@ -25,77 +50,30 @@ function Agreement(props) {
 
   return (
     <div className="flex flex-col items-center gap-3 mt-12 text-sm">
-      <div className="w-[500px] h-[42px] flex items-center rounded-lg bg-[#EAF4FF]">
+      <div className="w-[500px] h-[42px] rounded-lg bg-[#EAF4FF]">
         <input
+          id="agreeAll"
           type="checkbox"
-          className="mx-3"
+          className="hidden"
           checked={allChecked}
           onChange={allCheckedHandler}
         />
-        <span className="text-primary">전체동의(선택 항목 포함)</span>
+        <label htmlFor="agreeAll" className="flex items-center h-full">
+          <div className="relative w-3 h-3 mx-3 border border-darkgray">
+            {allChecked && <Check className="absolute -top-0.5 -left-0.5" />}
+          </div>
+          <span className="text-primary">전체동의(선택 항목 포함)</span>
+        </label>
       </div>
-      <div className="w-[500px] h-[42px] flex items-center rounded-lg">
-        <input
-          type="checkbox"
-          className="mx-3"
-          checked={checked[0]}
-          onChange={(e) => checkedHandler(e, 0)}
+      {agreementData.map((item) => (
+        <AgreeItem
+          key={item.id}
+          checked={checked[item.id]}
+          checkedHandler={checkedHandler}
+          index={item.id}
+          title={item.title}
         />
-        <span className="text-darkgray">이용약관 동의(필수)</span>
-        <span className="mr-3 ml-auto text-[#6F6F6F] text-xs underline underline-offset-2 cursor-pointer">
-          전문보기
-        </span>
-      </div>
-      <div className="w-[500px] h-[42px] flex items-center rounded-lg">
-        <input
-          type="checkbox"
-          className="mx-3"
-          checked={checked[1]}
-          onChange={(e) => checkedHandler(e, 1)}
-        />
-        <span className="text-darkgray">만 14세 이상 확인(필수)</span>
-        <span className="mr-3 ml-auto text-[#6F6F6F] text-xs underline underline-offset-2 cursor-pointer">
-          전문보기
-        </span>
-      </div>
-      <div className="w-[500px] h-[42px] flex items-center rounded-lg">
-        <input
-          type="checkbox"
-          className="mx-3"
-          checked={checked[2]}
-          onChange={(e) => checkedHandler(e, 2)}
-        />
-        <span className="text-darkgray">개인정보 수집 및 이용 동의(필수)</span>
-        <span className="mr-3 ml-auto text-[#6F6F6F] text-xs underline underline-offset-2 cursor-pointer">
-          전문보기
-        </span>
-      </div>
-      <div className="w-[500px] h-[42px] flex items-center rounded-lg">
-        <input
-          type="checkbox"
-          className="mx-3"
-          checked={checked[3]}
-          onChange={(e) => checkedHandler(e, 3)}
-        />
-        <span className="text-darkgray">마케팅 알림 수신 동의(선택)</span>
-        <span className="mr-3 ml-auto text-[#6F6F6F] text-xs underline underline-offset-2 cursor-pointer">
-          전문보기
-        </span>
-      </div>
-      <div className="w-[500px] h-[42px] flex items-center rounded-lg">
-        <input
-          type="checkbox"
-          className="mx-3"
-          checked={checked[4]}
-          onChange={(e) => checkedHandler(e, 4)}
-        />
-        <span className="text-darkgray">
-          위치기반 서비스 이용약관 동의(선택)
-        </span>
-        <span className="mr-3 ml-auto text-[#6F6F6F] text-xs underline underline-offset-2 cursor-pointer">
-          전문보기
-        </span>
-      </div>
+      ))}
     </div>
   );
 }

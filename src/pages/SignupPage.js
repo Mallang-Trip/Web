18a041667661ef5ff1bf5fb86f588de431d42657
DaeHee99 <mokday99@gmail.com>
2023-08-06 02:18:44 +0900

    feat: 회원가입 개인정보 입력

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import Logo from "../assets/images/logo.png";
 import Agreement from "../components/SignupPage/Agreement";
+import PersonalInfo from "../components/SignupPage/PersonalInfo";
 
 function SignupPage() {
+  const [step, setStep] = useState(0);
   const [activeNext, setActiveNext] = useState(false);
+
+  const nextClick = () => {
+    setStep(step + 1);
+    setActiveNext(false);
+  };
 
   return (
     <div className="h-screen">
@@ -11,7 +18,11 @@ function SignupPage() {
         <img src={Logo} className="h-9" alt="Mallang_Trip_Logo" />
       </div>
 
-      <Agreement setActiveNext={setActiveNext} />
+      {step === 0 ? (
+        <Agreement setActiveNext={setActiveNext} />
+      ) : (
+        <PersonalInfo setActiveNext={setActiveNext} />
+      )}
 
       <div className="flex justify-center mt-16">
         <button
@@ -21,6 +32,8 @@ function SignupPage() {
               ? "h-12 text-white rounded-full text-md w-80 bg-primary"
               : "h-12 bg-white border rounded-full text-darkgray text-md w-80 border-darkgray"
           }`}
+          disabled={!activeNext}
+          onClick={nextClick}
         >
           다음
         </button>

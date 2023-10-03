bc46121a33e94a96d8c32ed3d7979a5fb2c364d2
Daehee <mok9550@ajou.ac.kr>
2023-10-03 22:58:22 +0900

    fix: 제목 볼드 수정

--- src/pages/PartyPage/Atoms/PartyNumber/index.jsx
+++ src/pages/PartyPage/Atoms/PartyNumber/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PartyNumber() {
   return (
     <div className="pb-6">
-      <p className="text-lg">참여 인원</p>
+      <p className="text-lg font-bold">참여 인원</p>
       <p className="text-sm text-darkgray">2/4명</p>
     </div>
   );

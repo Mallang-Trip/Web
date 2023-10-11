bf72968d5f0647146652f6549c3eee25599c0f57
Daehee <mok9550@ajou.ac.kr>
2023-10-11 13:05:27 +0900

    feat: 목적지 선택 UI

--- src/pages/LandingPage/FirstBox/index.jsx
+++ src/pages/LandingPage/FirstBox/index.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import RegionModal from "./RegionModal";
 
 function FirstBox() {
+  const [showModal, setShowModal] = useState(false);
+  const [region, setRegion] = useState("아무데나");
+
+  const regionClickHandler = (target) => {
+    setRegion(target);
+    setShowModal(false);
+  };
+
   return (
-    <div className="w-full h-32 my-auto bg-white rounded-l-lg">
-      <p className="mt-3 mb-5 ml-3 text-gray-500 text-sm md:text-base text-gray">
-        목적지
-      </p>
-      <p className="text-lg md:text-xl text-black text-center">{"아무데나"}</p>
-    </div>
+    <>
+      <div
+        className="w-full h-32 my-auto bg-white rounded-l-lg cursor-pointer"
+        onClick={() => setShowModal(true)}
+      >
+        <p className="mt-3 mb-5 ml-3 text-gray-500 text-sm md:text-base text-gray">
+          목적지
+        </p>
+        <p className="text-lg md:text-xl text-black text-center">{region}</p>
+      </div>
+      <RegionModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        regionClickHandler={regionClickHandler}
+      />
+    </>
   );
 }
 

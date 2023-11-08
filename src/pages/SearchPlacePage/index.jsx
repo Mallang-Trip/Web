b207446a9900313020a4afaf0bfa7cbe81ca48bf
yurim222 <yurim@ajou.ac.kr>
2023-11-09 01:15:19 +0900

    feat: SearchPlacePage

--- src/pages/SearchPlacePage/index.jsx
+++ src/pages/SearchPlacePage/index.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import PlaceMap from "../../components/PlaceMap";
 import RoundBtn from "../../components/PlaceMap/RoundBtn";
 import ZimBtn from "../../components/PlaceMap/ZimBtn";
+import SearchBox from "../../components/PlaceMap/SearchBox";
 function SearchPlacePage() {
   return (
     <div>
       <div className="text-2xl pt-14 pb-3">가고 싶은 여행지를 찾아요</div>
       <div className="relative">
         <PlaceMap />
+        <div>
+          <SearchBox />
+        </div>
         <div className="absolute top-5 right-1/3 ">
           <RoundBtn name={"새로운 장소 추가"} />
         </div>

a591f605e781ae074e74c6b5f17ebaa3a2d3af3a
yurim222 <yurim@ajou.ac.kr>
2023-12-01 21:07:04 +0900

    Feat: Driver Page 개발

---
+++ src/pages/EditCoursePage/Revenue/index.jsx
@@ -0,0 +1,9 @@
+function Revenue() {
+  return (
+    <div>
+      <div className=" text-2xl font-bold">나의 수익</div>
+    </div>
+  );
+}
+
+export default Revenue;
--- src/pages/EditCoursePage/index.jsx
+++ src/pages/EditCoursePage/index.jsx
@@ -1,25 +1,68 @@
 import { useParams } from "react-router-dom";
 import { getCourseDetail } from "../../api/course";
+import { getPartyDetail } from "../../api/party";
 import PageContainer from "../../components/PageContainer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import HeadTitle from "../CourseSuggestPage/HeadTitle";
+import PartyImageBox from "../../components/PartyImageBox";
+import EditButton from "../../components/EditButton";
+import PartyPlan from "../../components/PartyPlan";
+import Period from "../PartyPage/Atoms/Period";
+import EditPeriod from "../../components/EditPeriod";
+import PartyNumber from "../PartyPage/Atoms/PartyNumber";
+import PersonLimit from "../../components/PersonLimit";
+import ToTalCredit from "../PartyPage/Atoms/ToTalCredit";
+import Revenue from "./Revenue";
+import Platform from "./Platform";
+import CourseDnD from "../CourseSuggestPage/CourseDnD";
 
 function EditCoursePage() {
-  let { courseId } = useParams();
+  const [images, setImages] = useState([]);
+  const [courseData, setCourseData] = useState({});
+  const [partyData, setPartyData] = useState({});
+  const { courseId } = useParams();
   const getCourseInfo = async () => {
     try {
       const result = await getCourseDetail(courseId);
-      console.log(result);
+      setImages(result.payload.images);
+      setCourseData(result.payload);
     } catch (e) {
       console.log(e);
     }
   };
-  useState(() => {
+  const getPartyData = async () => {
+    try {
+      const result = await getPartyDetail(courseId);
+      console.log(result.payload);
+      setPartyData(result.payload);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
     getCourseInfo();
-  }, [courseId]);
+  }, []);
+
+  useEffect(() => {
+    if (!partyData.partyId) return;
+    getPartyData();
+    console.log(partyData);
+  }, []);
+
+  if (!courseData.courseId) return null;
+
   return (
     <PageContainer>
       <HeadTitle name={"파티 수정하기"} isDriver={false} />
+      <PartyImageBox images={images} name={"gkgk"} />
+      <div className="flex justify-center items-center pt-6">
+        <EditButton title={"이미지 수정"} />
+      </div>
+      <EditPeriod />
+      <PersonLimit capacity={courseData.capacity} />
+      <ToTalCredit totalPrice={courseData.days[0].price} />
+      <Revenue />
+      <Platform />
     </PageContainer>
   );
 }

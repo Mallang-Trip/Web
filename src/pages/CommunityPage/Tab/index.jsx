112bd5bda63e132112ce880d31ed675cadba7c8a
Daehee <mok9550@ajou.ac.kr>
2023-12-06 01:35:22 +0900

    fix: 게시글 상세 페이지에서 탭 제거

--- src/pages/CommunityPage/Tab/index.jsx
+++ src/pages/CommunityPage/Tab/index.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 import TabBar from "./TabBar";
 import TabItem from "./TabItem";
 
 const TabList = ["전체", "자유게시판", "동행구해요", "피드백"];
 
 function Tab({ category, setCategory }) {
+  const { articleId } = useParams();
   const [offsetX, setOffsetX] = useState(0);
   const [offsetWidth, setOffsetWidth] = useState(34);
 
@@ -14,6 +16,7 @@ function Tab({ category, setCategory }) {
     setCategory(e.currentTarget.innerText);
   };
 
+  if (articleId !== "main") return null;
   return (
     <div className="flex flex-nowrap overflow-x-auto mt-2 mb-9 gap-5 text-center relative noScrollBar">
       <TabBar offsetX={offsetX} offsetWidth={offsetWidth} />

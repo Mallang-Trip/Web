fdb2d1032ee5124ad6e1bbfae2f4f8d065fb7534
Daehee <mok9550@ajou.ac.kr>
2023-12-13 15:10:51 +0900

    feat: 커뮤니티 게시글 무한스크롤 적용

--- src/pages/CommunityPage/index.jsx
+++ src/pages/CommunityPage/index.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getArticleList } from "../../api/article";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 import PageContainer from "../../components/PageContainer";
 import Title from "./Title";
 import Tab from "./Tab";
 import ArticleList from "../../components/ArticleList";
 import ArticleDetail from "./ArticleDetail";
+import Loading from "../../components/Loading";
 
 const articleType = {
   전체: "all",
@@ -18,23 +20,42 @@ function CommunityPage() {
   const { articleId } = useParams();
   const [category, setCategory] = useState("전체");
   const [articleData, setArticleData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(1);
+  const [endRef, isIntersecting] = useIntersectionObserver();
 
   const getArticleListFunc = async () => {
+    if (page >= totalPages) return;
+
     try {
-      const result = await getArticleList(articleType[category], 0);
-      setArticleData(result.payload.content);
+      const result = await getArticleList(articleType[category], page);
+      const articleDataCopy = page === 0 ? [] : [...articleData];
+      setTotalPages(result.payload.totalPages || 1);
+      setArticleData([...articleDataCopy, ...result.payload.content]);
     } catch (e) {
       console.log(e);
     }
   };
 
   useEffect(() => {
-    getArticleListFunc();
+    if (!isIntersecting) return;
+    setTimeout(() => setPage(page + 1), 1000);
+  }, [isIntersecting]);
 
+  useEffect(() => {
+    getArticleListFunc();
+  }, [page]);
+
+  useEffect(() => {
+    if (page === 0) getArticleListFunc();
+    setPage(0);
+  }, [category]);
+
+  useEffect(() => {
     window.scrollTo({
       top: 0,
     });
-  }, [articleId, category]);
+  }, [articleId]);
 
   return (
     <PageContainer>
@@ -45,6 +66,18 @@ function CommunityPage() {
       ) : (
         <ArticleDetail />
       )}
+      <div
+        ref={endRef}
+        className={`${
+          articleId !== "main" ||
+          articleData.length === 0 ||
+          page >= totalPages - 1
+            ? "hidden"
+            : "block"
+        }`}
+      >
+        <Loading full={false} />
+      </div>
     </PageContainer>
   );
 }

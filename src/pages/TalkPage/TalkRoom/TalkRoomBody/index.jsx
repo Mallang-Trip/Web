67f4327797ae2c73acc82a84d762b33512d1551d
Daehee <mok9550@ajou.ac.kr>
2023-12-22 16:56:02 +0900

    feat: 말랑톡 채팅방 스크롤 다운

--- src/pages/TalkPage/TalkRoom/TalkRoomBody/index.jsx
+++ src/pages/TalkPage/TalkRoom/TalkRoomBody/index.jsx
@@ -1,11 +1,50 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import TalkBubble from "./TalkBubble";
+import chatArrowIcon from "../../../../assets/svg/right-arrow.svg";
 
 function TalkRoomBody({ messages = [], setShowProfileModal }) {
+  const talkRoomRef = useRef();
   const user = useSelector((state) => state.user);
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const scrollDownHandler = () => {
+    const talkRoomSpace = talkRoomRef.current;
+    const { scrollHeight, clientHeight } = talkRoomSpace;
+
+    talkRoomSpace.scrollTo({
+      top: scrollHeight - clientHeight,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const talkRoomSpace = talkRoomRef.current;
+
+    const handleScroll = () => {
+      const { scrollHeight, clientHeight, scrollTop } = talkRoomSpace;
+      const isScrolledToBottom = scrollHeight - clientHeight <= scrollTop + 200;
+
+      setShowScrollButton(!isScrolledToBottom);
+    };
+
+    talkRoomSpace.addEventListener("scroll", handleScroll);
+
+    return () => {
+      talkRoomSpace.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (showScrollButton) return;
+    scrollDownHandler();
+  }, [messages]);
 
   return (
-    <div className="flex flex-col h-full overflow-y-auto px-2 noScrollBar">
+    <div
+      className="flex flex-col h-full overflow-y-auto px-2 noScrollBar"
+      ref={talkRoomRef}
+    >
       {messages.map((message, index) => (
         <TalkBubble
           key={message.messageId}
@@ -26,6 +65,22 @@ function TalkRoomBody({ messages = [], setShowProfileModal }) {
           {...message}
         />
       ))}
+      {showScrollButton && (
+        <div
+          className="fixed right-0 bottom-20 mr-3 flex justify-center animate-bounce"
+          style={{
+            width: `${talkRoomRef.current?.clientWidth}px`,
+          }}
+        >
+          <button onClick={scrollDownHandler}>
+            <img
+              src={chatArrowIcon}
+              alt="chat-down"
+              className="w-10 h-10 rounded-full rotate-90"
+            />
+          </button>
+        </div>
+      )}
     </div>
   );
 }

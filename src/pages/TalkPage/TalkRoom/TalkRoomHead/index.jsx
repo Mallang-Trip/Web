39ec1c373f8c94ab0c203ce4a241cb3a2e9b41d1
Daehee <mok9550@ajou.ac.kr>
2023-12-26 01:24:00 +0900

    fix: 말랑톡 닫기 버튼 수정

--- src/pages/TalkPage/TalkRoom/TalkRoomHead/index.jsx
+++ src/pages/TalkPage/TalkRoom/TalkRoomHead/index.jsx
@@ -4,13 +4,16 @@ import headerBack from "../../../../assets/svg/header-back.svg";
 function TalkRoomHead({ closeRoomHandler, name, setShowMenu }) {
   return (
     <div className="flex justify-between pl-1 pt-3 pb-2 h-16 border-b border-solid border-[#D9D9D9]">
-      <button
-        className="flex gap-2 items-center focus:outline-none"
-        onClick={closeRoomHandler}
-      >
-        <img src={headerBack} alt="back" className="w-5 h-5" />
+      <div className="flex gap-1 items-center">
+        <button
+          className="p-1 rounded-lg focus:outline-none hover:bg-darkgray/10"
+          onClick={closeRoomHandler}
+        >
+          <img src={headerBack} alt="back" className="w-5 h-5" />
+        </button>
         <span className="text-lg text-black font-bold">{name}</span>
-      </button>
+      </div>
+
       <button className="focus:outline-none" onClick={() => setShowMenu(true)}>
         <RxHamburgerMenu className="text-2xl mr-3" />
       </button>

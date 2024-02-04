00b150870f7b2959754b2d8ddf5fba8189544d63
Daehee <mok9550@ajou.ac.kr>
2024-02-05 02:25:00 +0900

    fix: 일반 회원용 서비스 이용약관 내용 수정

--- src/pages/PolicyPage/Component/PolicyDate/index.jsx
+++ src/pages/PolicyPage/Component/PolicyDate/index.jsx
@@ -1,9 +1,7 @@
 function PolicyDate() {
   return (
     <div className="mt-96 text-lg text-black font-bold">
-      약관 약관 제정 및 시행일 2023. 12. 1.
-      <br />
-      약관 개정일 2024. 8. 1. (시행 : 2024. 8. 10.) (개정 전 약관 보기)
+      약관 제정 및 시행일 2024.01.30
     </div>
   );
 }

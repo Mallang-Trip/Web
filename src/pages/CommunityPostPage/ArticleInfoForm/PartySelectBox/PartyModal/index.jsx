1fc2f38685f676757dcdb19689e8b90689e6b04c
Daehee <mok9550@ajou.ac.kr>
2024-03-14 22:55:23 +0900

    fix: 커뮤니티 게시글 작성시 파티 선택 모달 닫기 스타일 수정

--- src/pages/CommunityPostPage/ArticleInfoForm/PartySelectBox/PartyModal/index.jsx
+++ src/pages/CommunityPostPage/ArticleInfoForm/PartySelectBox/PartyModal/index.jsx
@@ -5,7 +5,6 @@ import PartyModalTab from "./PartyModalTab";
 import HeartList from "./HeartList";
 import NoPartyButton from "./NoPartyButton";
 import ReservationList from "./ReservationList";
-import closeIcon from "../../../../../assets/svg/close_x_primary.svg";
 
 function PartyModal({ showModal, setShowModal, setSelectedParty }) {
   const modalRef = useRef();
@@ -70,12 +69,25 @@ function PartyModal({ showModal, setShowModal, setSelectedParty }) {
     >
       <div className="m-auto shadow w-full max-w-[800px] h-[700px] px-8 py-9 bg-white rounded-lg relative">
         <p className="text-xl text-black font-bold">파티 선택</p>
-        <img
-          src={closeIcon}
-          alt="close"
-          className="absolute top-6 right-6 rounded-full hover:bg-skyblue cursor-pointer"
+        <button
+          type="button"
+          className="absolute top-6 right-6 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-black rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
           onClick={closeModal}
-        />
+        >
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
         <PartyModalTab isTabHeart={isTabHeart} setIsTabHeart={setIsTabHeart} />
         {isTabHeart ? (
           <HeartList

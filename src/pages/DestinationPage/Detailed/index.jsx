96a383801781b8b831ccb66afc319acd74d685b3
Daehee <mok9550@ajou.ac.kr>
2024-04-03 03:18:33 +0900

    fix: 여행지 상세 설명 줄바꿈 적용

--- src/pages/DestinationPage/Detailed/index.jsx
+++ src/pages/DestinationPage/Detailed/index.jsx
@@ -2,7 +2,9 @@ function Detailed({ content }) {
   return (
     <div className="flex flex-col gap-1 my-7">
       <p className="text-lg text-black font-bold">상세설명</p>
-      <div className="text-sm text-darkgray font-medium">{content}</div>
+      <div className="text-sm text-darkgray font-medium whitespace-pre">
+        {content}
+      </div>
     </div>
   );
 }

ec967efe3f8178750b452056d3df7907609e0a37
Daehee <mok9550@ajou.ac.kr>
2024-04-03 04:50:30 +0900

    fix: 드라이버 프로필 - 파티 코스 목록 디자인 수정

--- src/pages/MyProfilePage/DriverProfile/PartyCourse/Course/index.jsx
+++ src/pages/MyProfilePage/DriverProfile/PartyCourse/Course/index.jsx
@@ -1,24 +1,19 @@
-import { useNavigate } from "react-router-dom";
-
 function Course({ courseImg, courseName, courseId }) {
-  const navigate = useNavigate();
-  const onClickHandler = () => {
-    navigate(`/course/edit/${courseId}`);
-  };
   return (
-    <div
-      className="relative h-56 sm:h-64 cursor-pointer"
-      onClick={onClickHandler}
+    <button
+      className="relative h-56 sm:h-64"
+      onClick={() => console.log(courseId)}
     >
       <img
         className="absolute top-0 left-0 object-cover object-center w-full h-full overflow-hidden rounded-lg"
         src={courseImg}
-        alt="party-image"
+        alt={courseName}
       />
-      <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full text-lg sm:text-xl text-white">
+      <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full text-center text-lg sm:text-lg text-white font-bold z-10 px-3">
         {courseName}
       </div>
-    </div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 rounded-lg" />
+    </button>
   );
 }
 
--- src/pages/MyProfilePage/DriverProfile/PartyCourse/NewCourse/index.jsx
+++ src/pages/MyProfilePage/DriverProfile/PartyCourse/NewCourse/index.jsx
@@ -2,12 +2,12 @@ import primaryPlus from "../../../../../assets/svg/primary_plus.svg";
 
 function NewCourse() {
   return (
-    <div
-      className="h-56 sm:h-64 bg-skyblue border border-dashed border-primary rounded-lg cursor-pointer flex justify-center items-center"
+    <button
+      className="h-56 sm:h-64 bg-skyblue border border-dashed border-primary rounded-lg flex justify-center items-center"
       onClick={() => console.log("new_course")}
     >
       <img src={primaryPlus} alt="plus" className="w-6 h-6" />
-    </div>
+    </button>
   );
 }
 

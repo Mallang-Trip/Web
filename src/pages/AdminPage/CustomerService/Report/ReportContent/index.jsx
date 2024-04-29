29f7ffa6ebd8ba7564fba8c0b7db31b4a0eccd4d
seorinn <80616701+seorinn@users.noreply.github.com>
2024-04-30 01:37:18 +0900

    feat: 신고 내역 및 상세 페이지 구현, api(get) 적용

---
+++ src/pages/AdminPage/CustomerService/Report/ReportContent/index.jsx
@@ -0,0 +1,102 @@
+function ReportContent({ current, dataDetail, handleClose }) {
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Date(dateString)
+      .toLocaleDateString("ko-KR", options)
+      .replace(/\./g, "")
+      .split(" ")
+      .join(".");
+  };
+  return (
+    <div className="cursor-default">
+      <div className="flex flex-col">
+        <div className="flex items-center mb-2">
+          <div className="text-2xl font-bold mr-2">
+            {current === 0 ? "신고 내용" : "처리 완료 내용"}
+          </div>
+          <div className="text-boldgray text-sm font-medium">
+            {formatDate(dataDetail.createdAt)}
+          </div>
+        </div>
+        <div className="flex mb-12">
+          <button
+            className="text-gray500 text-sm font-medium"
+            onClick={handleClose}
+          >
+            신고 당한 페이지로 이동
+          </button>
+        </div>
+        <div className="flex">
+          <div className="flex cursor-pointer items-center px-5 py-3 text-sm font-bold rounded-xl bg-[#FFEAEA] mr-3">
+            피신고자
+            <hr className="w-[0.0625rem] h-4 mx-3 bg-gray400" />
+            <div className="text-[#F00] mr-1">
+              {dataDetail.reporteeNickname}
+            </div>
+            ({dataDetail.reporteeId})
+            {current !== 0 && (
+              <hr className="w-[0.0625rem] h-4 mx-3 bg-gray400" />
+            )}
+            {current !== 0 && <div className="text-[#F00]">말랑톡</div>}
+          </div>
+          <div className="flex cursor-pointer items-center px-5 py-3 text-sm font-bold rounded-xl bg-skyblue">
+            신고자
+            <hr className="w-[0.0625rem] h-4 mx-3 bg-gray400" />
+            <div className="text-primary mr-1">
+              {dataDetail.reporteeNickname}
+            </div>
+            ({dataDetail.reporteeId})
+            <hr className="w-[0.0625rem] h-4 mx-3 bg-gray400" />
+            <div className="text-primary">말랑톡</div>
+          </div>
+        </div>
+        {current === 0 ? (
+          <div className="w-full h-72 max-h-fit mt-4 mb-8 border border-gray300 p-7 text-gray700 text-base font-medium">
+            {dataDetail.content}
+          </div>
+        ) : (
+          <div className="flex flex-col mt-10">
+            <div className="flex w-full mb-5">
+              <div className="flex items-center w-fit h-16 px-3 py-5 bg-lightgray rounded-xl mr-3 text-xs font-semibold">
+                신고 내용
+              </div>
+              <div className="px-4 py-5 text-base font-medium border border-gray300 rounded-xl flex-1">
+                {dataDetail.suspensionContent}
+              </div>
+            </div>
+            <div className="flex w-full mb-5">
+              <div className="flex items-center w-fit h-16 px-3 py-5 bg-lightgray rounded-xl mr-3 text-xs font-semibold">
+                제재 사유
+              </div>
+              <div className="px-4 py-5 text-base font-medium border border-gray300 rounded-xl flex-1">
+                {dataDetail.suspensionContent}
+              </div>
+            </div>
+            <div className="flex w-full mb-5">
+              <div className="flex items-center w-fit h-16 px-3 py-5 bg-lightgray rounded-xl mr-3 text-xs font-semibold">
+                제재 여부
+              </div>
+              <div
+                className={`px-4 py-5 text-base font-medium border border-gray300 rounded-xl flex-1 ${dataDetail.suspensionExist ? "text-primary" : "text-[#F00]"}`}
+              >
+                {dataDetail.suspensionExist ? "O" : "X"}
+              </div>
+            </div>
+          </div>
+        )}
+        {current === 0 && (
+          <div className="flex justify-center">
+            <button
+              className="w-80 h-12 bg-primary text-white text-xl font-bold rounded-full px-5 py-1"
+              onClick={handleClose}
+            >
+              처리 완료
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ReportContent;
--- src/pages/AdminPage/CustomerService/Report/index.jsx
+++ src/pages/AdminPage/CustomerService/Report/index.jsx
@@ -1,43 +1,152 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  getReportList,
+  getReportDetail,
+  getReportCompleteList,
+  getReportCompleteDetail,
+} from "../../../../api/admin";
+import Loading from "../../../../components/Loading";
 import TabList from "../../../../components/Admin/TabList";
-import Table from "../../../../components/Admin/Table";
+import ReportContent from "./ReportContent";
 
 function Report() {
+  const [loading, setLoading] = useState(true);
+  const [showReportContent, setShowReportContent] = useState(false);
+  const cols = [
+    ["피신고자 닉네임", "아이디", "신고날짜", "프로필"],
+    ["닉네임", "아이디", "제재 사유", "제재 여부"],
+  ];
   const [current, setCurrent] = useState(0);
+  const [column, setColumn] = useState(cols[0]);
+  const [dataWaiting, setDataWaiting] = useState();
+  const [dataDone, setDataDone] = useState();
+  const [dataList, setDataList] = useState();
+  const [dataDetail, setDataDetail] = useState();
+
+  const getReportListFunc = async (c) => {
+    try {
+      const result1 = await getReportList();
+      const result2 = await getReportCompleteList();
+      setDataWaiting(result1.payload);
+      setDataDone(result2.payload);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const getReportDetailFunc = async (detailId) => {
+    try {
+      const result =
+        current === 0
+          ? await getReportDetail(detailId)
+          : await getReportCompleteDetail(detailId);
+      setDataDetail(result.payload);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getReportListFunc();
+  }, []);
+
+  useEffect(() => {
+    setColumn(cols[current]);
+    if (current === 0) setDataList(dataWaiting);
+    else setDataList(dataDone);
+  }, [current]);
+
   const tabList = [
     { name: "신고처리 대기", id: "waiting" },
     { name: "처리 완료 목록", id: "done" },
   ];
-  const columnsWaiting = ["피신고자 닉네임", "아이디", "신고날짜", "프로필"];
-  const columnsDone = ["닉네임", "아이디", "제재 사유", "제재 여부"];
-  const dataWaiting = [
-    ["PrimeMinister", "id1234", "2023-09-30"],
-    ["NAVYDI", "id1234", "2023-09-30"],
-    ["메루옹", "id1234", "2023-09-30"],
-    ["하이요123", "id1234", "2023-09-30"],
-    ["누구안녕하세여", "id1234", "2023-09-30"],
-    ["qweR", "id1234", "2023-09-30"],
-    ["열글자닉네임임임임", "id1234", "2023-09-30"],
-  ];
-  const dataDone = [
-    ["PrimeMinister", "id1234", "욕설, 비난", 1],
-    ["NAVYDI", "id1234", "성적인 발언", 1],
-    ["메루옹", "id1234", "비하 발언", 1],
-    ["하이요123", "id1234", "", 0],
-  ];
-
   const changeTab = (current) => {
     setCurrent(current);
   };
+  const setWidth = (index) => {
+    if (current === 0) {
+      if (index === 0) return "w-1/5";
+      else if (index === 1) return "flex-1";
+      else if (index === 2) return "w-[12.5%]";
+      else return "w-16 flex justify-end";
+    } else {
+      if (index === 0) return "w-1/5";
+      else if (index === 1) return "w-1/5";
+      else if (index === 2) return "flex-1";
+      else return "w-16";
+    }
+  };
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Date(dateString)
+      .toLocaleDateString("ko-KR", options)
+      .replace(/\./g, "")
+      .split(" ")
+      .join(".");
+  };
+  const suspensionExistFunc = (suspensionExist) => {
+    if (suspensionExist)
+      return <div className="text-primary font-semibold">O</div>;
+    else return <div className="text-[#F00] font-semibold">X</div>;
+  };
 
+  if (loading) return <Loading full={true} />;
+  else if (showReportContent)
+    return (
+      <ReportContent
+        current={current}
+        dataDetail={dataDetail}
+        handleClose={() => setShowReportContent(false)}
+      />
+    );
   return (
-    <div>
+    <div className="cursor-default">
       <div className="text-2xl font-bold">신고 내역</div>
       <TabList tabList={tabList} changeTab={changeTab} />
-      <Table
-        columns={current === 0 ? columnsWaiting : columnsDone}
-        data={current === 0 ? dataWaiting : dataDone}
-      />
+      <div className="flex flex-col w-full">
+        <div className="flex items-center w-full px-5 py-3 justify-center bg-[#EAF4FF] rounded-xl mb-2 text-sm text-[#313033]">
+          {column.map((item, index) => (
+            <div key={index} className={`font-semibold ${setWidth(index)}`}>
+              {item}
+            </div>
+          ))}
+        </div>
+        {dataList &&
+          dataList.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center w-full px-5 py-3 h-10 border border-solid border-[#EFEFEF] rounded-xl mb-2 text-sm font-medium text-gray700 cursor-pointer"
+              onClick={() => {
+                setShowReportContent(true);
+                getReportDetailFunc(item.reportId);
+              }}
+            >
+              <div className="w-1/5">{item.reporteeNickname}</div>
+              <div
+                className={`flex items-center relative left-[-.75rem] pr-3 ${current === 0 ? "flex-1" : "w-1/5"}`}
+              >
+                <hr className="w-[0.0625rem] h-4 mr-3 bg-gray400" />
+                {item.reportId}
+              </div>
+              <div
+                className={`${current === 0 ? "w-[12.5%] text-gray500" : "flex-1"}`}
+              >
+                {current === 0
+                  ? formatDate(item.createdAt)
+                  : item.suspensionContent}
+              </div>
+              {current === 0 && <hr className="w-[0.0625rem] h-4 bg-gray400" />}
+              <div
+                className={`flex w-16 ${current === 0 ? "justify-end text-gray500 cursor-pointer" : ""}`}
+              >
+                {current === 0
+                  ? "프로필"
+                  : suspensionExistFunc(item.suspensionExist)}
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }

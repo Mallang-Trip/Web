c7b31bba7433a6d09d2316e6807f490389aa2f98
Daehee <mok9550@ajou.ac.kr>
2024-05-01 04:21:07 +0900

    fix: 탭 전환 효과

--- src/pages/LandingPage/DriverHome/DriverTab/index.jsx
+++ src/pages/LandingPage/DriverHome/DriverTab/index.jsx
@@ -2,19 +2,19 @@ function DriverTab({ tab, setTab }) {
   return (
     <div className="w-full grid grid-cols-3 rounded-lg">
       <button
-        className={`h-12 rounded-l-lg border border-primary text-sm font-semibold ${tab === 0 ? "text-white bg-primary" : "text-primary bg-white"}`}
+        className={`h-12 rounded-l-lg border border-primary text-sm font-semibold transition-all duration-300 ${tab === 0 ? "text-white bg-primary" : "text-primary bg-white"}`}
         onClick={() => setTab(0)}
       >
         예약된 파티
       </button>
       <button
-        className={`h-12 border-y border-primary text-sm font-semibold ${tab === 1 ? "text-white bg-primary" : "text-primary bg-white"}`}
+        className={`h-12 border-y border-primary text-sm font-semibold transition-all duration-300 ${tab === 1 ? "text-white bg-primary" : "text-primary bg-white"}`}
         onClick={() => setTab(1)}
       >
         가입된 파티
       </button>
       <button
-        className={`h-12 rounded-r-lg border border-primary text-sm font-semibold ${tab === 2 ? "text-white bg-primary" : "text-primary bg-white"}`}
+        className={`h-12 rounded-r-lg border border-primary text-sm font-semibold transition-all duration-300 ${tab === 2 ? "text-white bg-primary" : "text-primary bg-white"}`}
         onClick={() => setTab(2)}
       >
         새로운 파티 신청

58e6a65f5928b812dd2da597683b2fc6c5d1666d
Daehee <mok9550@ajou.ac.kr>
2024-05-02 18:43:06 +0900

    fix: 카카오톡 상담, 새로운 파티 만들기 버튼 디자인 수정

--- src/pages/LandingPage/KakaoButton/index.jsx
+++ src/pages/LandingPage/KakaoButton/index.jsx
@@ -1,18 +1,14 @@
 import { Link } from "react-router-dom";
-import kakaoIcon from "../../../assets/images/kakaoIcon.png";
+import kakaoIcon from "../../../assets/svg/kakao-icon.svg";
 
 function KakaoButton() {
   return (
     <Link
       to="http://pf.kakao.com/_tfMxaG/chat"
       target="_blank"
-      className="flex items-center justify-center fixed bottom-24 right-6 lg:right-12 xl:right-40 rounded-full w-14 h-14 focus:outline-none"
+      className="flex items-center justify-center fixed bottom-40 right-6 lg:right-12 xl:right-40 bg-[#FEE500] rounded-[20px] w-14 h-14 focus:outline-none"
     >
-      <img
-        src={kakaoIcon}
-        className="w-full h-full rounded-full"
-        alt="카카오톡 문의"
-      />
+      <img src={kakaoIcon} alt="카카오톡 문의" />
     </Link>
   );
 }

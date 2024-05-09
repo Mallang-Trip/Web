6b77c2a305555493858acc3ca61cbcf1322c187f
Daehee <mok9550@ajou.ac.kr>
2024-05-10 03:20:32 +0900

    fix: 고객센터 페이지 상담 컴포넌트 위치 수정

--- src/pages/HelpPage/Chatting/index.jsx
+++ src/pages/HelpPage/Chatting/index.jsx
@@ -26,7 +26,7 @@ function Chatting() {
 
   return (
     <>
-      <div className="max-w-[550px] mx-auto py-5 flex flex-col items-center gap-6 bg-skyblue rounded-xl">
+      <div className="max-w-[550px] mx-auto mt-40 py-5 flex flex-col items-center gap-6 bg-skyblue rounded-xl">
         <div className="text-xl text-black font-bold">1:1 채팅상담</div>
         <div className="text-sm text-darkgray font-medium">
           채팅상담은 공휴일을 제외한 평일 10:00~18:00에 운영됩니다.

5274ce5e5796292484e6db4f620bbc4d72b4fd8a
Daehee <mok9550@ajou.ac.kr>
2024-05-15 20:27:22 +0900

    chore: PWA Cache 설정

--- sw.js
+++ sw.js
@@ -1,14 +1,44 @@
+const CACHE_NAME = "pwa-task-manager";
+const urlsToCache = ["/"];
+
 // install event
 self.addEventListener("install", (e) => {
-  // console.log("[Service Worker] installed");
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 // activate event
 self.addEventListener("activate", (e) => {
-  // console.log("[Service Worker] actived", e);
+  const cacheWhitelist = [CACHE_NAME];
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 // fetch event
 self.addEventListener("fetch", (e) => {
-  // console.log("[Service Worker] fetched resource " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      return (
+        r ||
+        fetch(e.request).then(async (response) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
 });

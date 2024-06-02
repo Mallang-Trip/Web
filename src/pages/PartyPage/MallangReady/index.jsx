80b7c5886d4d65e320ed6a805118b6a408a3d5ae
seorinn <80616701+seorinn@users.noreply.github.com>
2024-06-02 16:38:19 +0900

    fix: 마지막 말랑레디 시 checkModal 추가 및 로딩 처리

--- src/pages/PartyPage/MallangReady/index.jsx
+++ src/pages/PartyPage/MallangReady/index.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { putMallangReady } from "../../../api/party";
 import { computeGapDay } from "../../../utils";
+import Loading from "../../../components/Loading";
+import CheckModal from "../../../components/CheckModal";
 import WhatReady from "./WhatReady";
 
 function MallangReady({
@@ -14,15 +16,25 @@ function MallangReady({
 }) {
   const user = useSelector((state) => state.user);
   const { partyId } = useParams();
+  const [loading, setLoading] = useState(false);
   const [ready, setReady] = useState(false);
+  const [isLast, setIsLast] = useState(false);
+  const [showCheckModal, setShowCheckModal] = useState(false);
 
   const readyClickHandler = async () => {
     try {
-      await putMallangReady(partyId, !ready);
-      setReady(!ready);
-      getPartyData();
+      if (isLast && !ready) {
+        setShowCheckModal(true);
+      } else {
+        if (isLast) setLoading(true);
+        await putMallangReady(partyId, !ready);
+        setReady(!ready);
+        getPartyData();
+      }
     } catch (e) {
       console.log(e);
+    } finally {
+      if (partyStatus !== "RECRUITING") setLoading(false);
     }
   };
 
@@ -39,8 +51,13 @@ function MallangReady({
           partyStatus === "SEALED" ||
           partyStatus === "WAITING_COURSE_CHANGE_APPROVAL"
       );
+    const readyMembers = members.filter((member) => member.ready);
+    if (readyMembers.length === members.length - 1 && !ready) {
+      setIsLast(true);
+    }
   }, []);
 
+  if (loading) return <Loading full={true} />;
   if (computeGapDay(startDate) <= 0 && partyStatus !== "RECRUITING")
     return null;
   return (
@@ -64,6 +81,17 @@ function MallangReady({
         </button>
       </div>
       <WhatReady />
+      <CheckModal
+        showModal={showCheckModal}
+        setShowModal={setShowCheckModal}
+        message={`말랑레디를 누르시겠습니까?\n\n누르는 순간 예약이 확정되며\n결제가 즉시 진행됩니다`}
+        noText="아니오"
+        yesText="예"
+        yesHandler={() => {
+          setReady(!ready);
+          readyClickHandler();
+        }}
+      />
     </div>
   );
 }

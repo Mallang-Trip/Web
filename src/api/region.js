609bc4a289431f666c76855a9cea75bff0c9931b
seorinn <seorin@ajou.ac.kr>
2024-07-03 20:17:03 +0900

    fix: region api url 주소 변경

--- src/api/region.js
+++ src/api/region.js
@@ -3,13 +3,13 @@ import { GET, POST, PUT, DELETE } from "../utils/axios";
 export const getPartyRegionList = async () => await GET("/party/region");
 
 export const postNewPartyRegion = async (body) =>
-  await POST("/party/region", body, true);
+  await POST("/region", body, true);
 
 export const putPartyRegion = async (partyRegionId, body) =>
-  await PUT(`/party/region/${partyRegionId}`, body, true);
+  await PUT(`/region/${partyRegionId}`, body, true);
 
 export const deletePartyRegion = async (partyRegionId) =>
-  await DELETE(`/party/region/${partyRegionId}`, true);
+  await DELETE(`/region/${partyRegionId}`, true);
 
 export const getPartyRegionDriverList = async (partyRegionId) =>
-  await GET(`/party/region/${partyRegionId}`, true);
+  await GET(`/region/${partyRegionId}`, true);

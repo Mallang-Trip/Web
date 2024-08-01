8521ec063af4fc2b573708e680e26ec5d457896e
Daehee <mok9550@ajou.ac.kr>
2024-08-01 23:42:36 +0900

    feat: 코스 수정 버튼 디자인 변경

--- src/components/PartyPlan/PlanBox/index.jsx
+++ src/components/PartyPlan/PlanBox/index.jsx
@@ -1,7 +1,7 @@
 function PlanBox({ index, name, time }) {
   return (
     <div className="relative w-full h-16 md:h-[83px] mb-6 rounded-full bg-skyblue font-bold">
-      <div className="w-10 h-10 md:w-14 md:h-14 absolute left-4 top-3 rounded-full bg-primary text-white text-lg flex justify-center items-center">
+      <div className="w-10 h-10 md:w-14 md:h-14 absolute left-4 top-1/2 -translate-y-1/2 rounded-full bg-primary text-white text-lg flex justify-center items-center">
         {index}
       </div>
       <div className="h-full flex justify-between ml-16 md:ml-24 mr-5 md:mr-10 text-boldblue text-md md:text-xl items-center">

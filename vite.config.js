cb259e39d185e81062f2e48568364fb023eed1a2
Daehee <mok9550@ajou.ac.kr>
2024-08-05 00:47:24 +0900

    chore: PASS resource url 환경 변수 분리

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,40 @@
-import { defineConfig } from "vite";
+import { loadEnv } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        // svgr options
-      },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      svgr({
+        svgrOptions: {
+          // svgr options
+        },
+      }),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            VITE_PASS_RESOURCE_URL: env.VITE_PASS_RESOURCE_URL,
+          },
+        },
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+        },
       },
     },
-  },
-  optimizeDeps: {
-    exclude: ["js-big-decimal"],
-  },
-  define: {
-    global: "globalThis",
-  },
-});
+    optimizeDeps: {
+      exclude: ["js-big-decimal"],
+    },
+    define: {
+      global: "globalThis",
+    },
+  };
+};

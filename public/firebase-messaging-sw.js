d91977ccbca07a88755c3ea75f97ad2cc609b999
Daehee <mok9550@ajou.ac.kr>
2024-08-07 01:44:45 +0900

    feat: 말랑챗 푸시 알림 사용성 개선

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,17 +4,41 @@ self.addEventListener("push", (e) => {
   const resultData = e.data.json().notification;
   const tag = e.data.json().data.tag;
 
+  const isImageChat =
+    resultData.body.startsWith(
+      "https://mallang-trip-db.s3.ap-northeast-2.amazonaws.com"
+    ) && tag.startsWith("/talk?chatRoomId");
+
   const notificationTitle = resultData.title;
   const notificationOptions = {
-    body: resultData.body,
-    icon: "https://mallang-trip-db.s3.ap-northeast-2.amazonaws.com/profile/ae54463f-1299-4caa-b763-a94f5b300e27mallangtrip.png",
-    image:
-      "https://mallang-trip-db.s3.ap-northeast-2.amazonaws.com/profile/9a360955-8f22-4911-9708-53b1065f9b5amallangtrip.png",
-    tag: tag,
     ...resultData,
+    body: isImageChat ? "사진을 전송하였습니다." : resultData.body,
+    icon: "https://mallang-trip-db.s3.ap-northeast-2.amazonaws.com/profile/ae54463f-1299-4caa-b763-a94f5b300e27mallangtrip.png",
+    image: isImageChat
+      ? resultData.body
+      : "https://mallang-trip-db.s3.ap-northeast-2.amazonaws.com/profile/9a360955-8f22-4911-9708-53b1065f9b5amallangtrip.png",
+    tag: tag,
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  e.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        const isVisible = clientList.some((client) => {
+          return (
+            client.visibilityState === "visible" &&
+            client.url.indexOf("/talk") > 0
+          );
+        });
+
+        if (!isVisible) {
+          return self.registration.showNotification(
+            notificationTitle,
+            notificationOptions
+          );
+        }
+      })
+  );
 });
 
 self.addEventListener("notificationclick", (e) => {

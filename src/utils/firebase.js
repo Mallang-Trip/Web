cba40ffea069593062a89f0064ec7d2047dc96fa
Daehee <mok9550@ajou.ac.kr>
2024-08-20 00:04:51 +0900

    fix: firebase analytics 제거

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
@@ -13,6 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const messaging =
   "serviceWorker" in navigator ? getMessaging(app) : undefined;

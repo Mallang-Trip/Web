bf60a893b6a68334fa4ae386e7a31f6568b0cee3
seorinn <seorin@ajou.ac.kr>
2024-09-04 22:12:03 +0900

    feat: 드라이버 정보 내 차량 이미지 확대 기능 추가
    fix: 프로필 수정 시 x 버튼을 눌러야만 삭제 되도록 수정

--- src/pages/MyProfilePage/DriverProfile/Vehicle/ImageInput/index.jsx
+++ src/pages/MyProfilePage/DriverProfile/Vehicle/ImageInput/index.jsx
@@ -3,7 +3,7 @@ import primaryPlus from "../../../../../assets/svg/primary_plus.svg";
 function ImageInput({ vehicleImageRef, vehicleImageHandler }) {
   return (
     <div
-      className="shrink-0 w-48 h-full bg-skyblue border border-dashed border-primary rounded-lg cursor-pointer flex justify-center items-center"
+      className="shrink-0 w-1/4 h-full bg-skyblue border border-dashed border-primary rounded-lg cursor-pointer flex justify-center items-center"
       onClick={() => vehicleImageRef.current.click()}
     >
       <img src={primaryPlus} alt="plus" className="w-4 h-4" />

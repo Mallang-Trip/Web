71e26f2e92570ff367b8312b674f291f35a7a43b
@u_limit_ <yurim@ajou.ac.kr>
2024-09-10 19:19:59 +0900

    feat: 회원 수 기능 추가 및 코드 리팩터링

--- src/pages/AdminPage/KPI/Container/index.jsx
+++ src/pages/AdminPage/KPI/Container/index.jsx
@@ -1,17 +1,19 @@
 function Container({ name, child }) {
-  console.log(name, child);
+  const unit = name.endsWith("수") ? " 회" : name.endsWith("률") ? "%" : "명";
+
   return (
     <div className="whitespace-nowrap mb-12">
       <div className="text-boldgray text-lg font-semibold mb-2">{name}</div>
       <div className="grid grid-cols-3 gap-12 border-solid border border-[#D9D9D9] rounded-2xl px-12 py-10 w-full min-w-max">
-        {child.map((item, index) => (
+        {child.map(([label, value], index) => (
           <div
             key={index}
             className="flex flex-col text-[#3E3E3E] text-lg font-bold"
           >
-            <div className="mb-2">{item[0]}</div>
+            <div className="mb-2">{label}</div>
             <div className="flex text-[#1C1B1F] text-3xl font-bold">
-              0{name === "다운로드" ? " 회" : " %"}
+              {value}
+              {unit}
             </div>
           </div>
         ))}

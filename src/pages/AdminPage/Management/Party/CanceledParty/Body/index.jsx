0442d9dc67b1689f3298c6465f957f423d03534d
Daehee <mok9550@ajou.ac.kr>
2024-09-12 00:49:52 +0900

    feat: 관리자 프로모션 파티 확인

--- src/pages/AdminPage/Management/Party/CanceledParty/Body/index.jsx
+++ src/pages/AdminPage/Management/Party/CanceledParty/Body/index.jsx
@@ -1,4 +1,6 @@
+import { memo } from "react";
 import { useNavigate } from "react-router-dom";
+import clsx from "clsx";
 
 function Body({
   partyId,
@@ -6,6 +8,7 @@ function Body({
   startDate,
   headcount,
   capacity,
+  promotion,
   driverName,
   status,
 }) {
@@ -19,7 +22,7 @@ function Body({
   };
 
   return (
-    <div className="w-full py-3 grid grid-cols-5 items-center text-center bg-white border border-gray300 rounded-xl">
+    <div className="w-full py-3 grid grid-cols-6 items-center text-center bg-white border border-gray300 rounded-xl">
       <p
         className="px-1 cursor-pointer"
         onClick={() => navigation(`/admin/party?party_id=${partyId}`)}
@@ -32,10 +35,18 @@ function Body({
       <p className="px-1 text-gray500 font-medium">
         {headcount}/{capacity} 명
       </p>
+      <p
+        className={clsx(
+          "px-1 font-medium",
+          promotion ? "text-primary" : "text-gray500"
+        )}
+      >
+        {promotion ? "O" : "X"}
+      </p>
       <p className="px-1 text-[#FF0000]">{statusObj[status] || status}</p>
       <p className="px-1 text-gray500">{driverName}</p>
     </div>
   );
 }
 
-export default Body;
+export default memo(Body);

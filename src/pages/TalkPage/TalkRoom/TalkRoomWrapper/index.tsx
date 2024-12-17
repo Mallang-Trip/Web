68d3b08b09bdaf61a597efdbda9f4b333ec97d12
Daehee <mok9550@ajou.ac.kr>
2024-12-18 00:07:06 +0900

    fix: 채팅방 스크롤 수정

--- src/pages/TalkPage/TalkRoom/TalkRoomWrapper/index.tsx
+++ src/pages/TalkPage/TalkRoom/TalkRoomWrapper/index.tsx
@@ -39,9 +39,16 @@ function TalkRoomWrapper({ children }: Props) {
     }
   }, []);
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
     <div
-      className="flex flex-col justify-between gap-3 h-full pb-3 px-1 bg-white rounded-lg border border-mediumgray m-3 z-50 relative"
+      className="flex flex-col justify-between gap-3 pb-3 px-1 bg-white rounded-lg border border-mediumgray m-3 z-50 relative"
       style={{
         height: styleHeight,
       }}
